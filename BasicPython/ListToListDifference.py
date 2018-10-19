e9787b11805c37893f0487be708a808545a169ef
shruti <shruti.kerkar@bridgelabz.com>
2018-10-19 13:17:51 +0530

    Updated commit

---
+++ BasicPython/ListToListDifference.py
@@ -0,0 +1,11 @@
+
+# Write a Python program to get the difference between the two lists.
+
+list1=[1,2]
+list2=[2,3]
+
+Diffrence=set(list1) - set(list2)
+print(Diffrence)
+
+Symmetric_Diffrence=set(list1).symmetric_difference(set(list2))
+print(Symmetric_Diffrence)
