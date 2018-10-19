f48160653700921b49368f2d1e76c66d58a653ca
shruti <shruti.kerkar@bridgelabz.com>
2018-10-19 13:19:34 +0530

    Updated commit

--- BasicPython/ListToListDifference.py
+++ BasicPython/ListToListDifference.py
@@ -6,6 +6,7 @@ list2=[2,3]
 
 Diffrence=set(list1) - set(list2)
 print(Diffrence)
-
+print(list(Diffrence))
 Symmetric_Diffrence=set(list1).symmetric_difference(set(list2))
 print(Symmetric_Diffrence)
+print(list(Symmetric_Diffrence))
