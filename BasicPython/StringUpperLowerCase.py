d8a480e2efad1377d8ff9ebf543dc96e80d85951
shruti <shruti.kerkar@bridgelabz.com>
2018-10-20 12:08:20 +0530

    Updated commit

---
+++ BasicPython/StringUpperLowerCase.py
@@ -0,0 +1,9 @@
+
+#Write a Python script that takes input from the user and displays that input back in upper and lower cases.
+
+
+Input_string="string upper case"
+
+print(Input_string.upper())
+print(Input_string.lower())
+
