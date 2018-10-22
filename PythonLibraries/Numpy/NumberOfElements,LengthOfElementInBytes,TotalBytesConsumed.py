cc1d48f04d08c9ab4fec9fac8deae94510606df1
shruti <shruti.kerkar@bridgelabz.com>
2018-10-22 15:43:31 +0530

    Updated commit

---
+++ PythonLibraries/Numpy/NumberOfElements,LengthOfElementInBytes,TotalBytesConsumed.py
@@ -0,0 +1,14 @@
+
+#Write a Python program to find the number of elements of an array, length of one array element in bytes and total bytes consumed by the elements.
+#Expected Output:
+#Size of the array: 3
+#Length of one array element in bytes: 8
+#Total bytes consumed by the elements of the array: 24
+
+import numpy as np
+
+x=np.array([1,2,3,4],dtype=np.float)
+print(x)
+print("Size of array:",x.size)
+print("Length of one array element in bytes:",x.itemsize)
+print("Total bytes consumed by the elements of the array:",x.nbytes)
