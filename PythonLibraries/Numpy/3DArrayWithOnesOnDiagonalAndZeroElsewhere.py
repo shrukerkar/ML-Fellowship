959c94676d5e2e598851dbfc4deebe17954c8739
shruti <shruti.kerkar@bridgelabz.com>
2018-10-22 17:12:12 +0530

    Updated commit

---
+++ PythonLibraries/Numpy/3DArrayWithOnesOnDiagonalAndZeroElsewhere.py
@@ -0,0 +1,11 @@
+
+#Write a Python program to create a 3-D array with ones on a diagonal and zeros elsewhere.
+#Expected Output:
+#[[ 1. 0. 0.]
+#[ 0. 1. 0.]
+#[ 0. 0. 1.]]
+
+import numpy as np
+
+a=np.eye(3)
+print(a)
