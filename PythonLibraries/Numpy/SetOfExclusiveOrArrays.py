738f2ce7c32bcd593f6d904184b12752d6961950
shruti <shruti.kerkar@bridgelabz.com>
2018-10-22 16:16:58 +0530

    Updated commit

---
+++ PythonLibraries/Numpy/SetOfExclusiveOrArrays.py
@@ -0,0 +1,18 @@
+
+# Write a Python program to find the set exclusive-or of two arrays. Set exclusive-or will return the sorted, unique values that are in only one (not both) of the input arrays.
+#Array1: [ 0 10 20 40 60 80]
+#Array2: [10, 30, 40, 50, 70]
+#Unique values that are in only one (not both) of the input arrays:
+#[ 0 20 30 50 60 70 80]
+
+import numpy as np
+
+array1 = np.array([0, 10, 20, 40, 60, 80])
+print("Array1: ",array1)
+
+array2 = [10, 30, 40, 50, 70]
+print("Array2: ",array2)
+
+print("Unique values that are in only one (not both) of the input arrays:")
+
+print(np.setxor1d(array1, array2))
