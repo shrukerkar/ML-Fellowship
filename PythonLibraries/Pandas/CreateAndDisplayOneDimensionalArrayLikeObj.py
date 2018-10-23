a266a8ab4b9ae59e2eb14a888a934f019fb98621
shruti <shruti.kerkar@bridgelabz.com>
2018-10-23 12:45:33 +0530

    Updated commit

---
+++ PythonLibraries/Pandas/CreateAndDisplayOneDimensionalArrayLikeObj.py
@@ -0,0 +1,8 @@
+
+#Write a Python program to create and display a one-dimensional array-like object containing an array of data using Pandas module.
+
+import pandas as pd
+
+df = pd.Series([2, 4, 6, 8, 10])
+
+print(df)
