91bc146955616e4f6b510822bff8b7ee1a5bac7f
shruti <shruti.kerkar@bridgelabz.com>
2018-10-23 16:09:33 +0530

    Updated commit

---
+++ PythonLibraries/Pandas/PowersOfArrayValuesElementwise.py
@@ -0,0 +1,12 @@
+
+#. Write a Python program to get the powers of an array values element-wise.
+#Note: First array elements raised to powers from second array
+#Expected Output:
+#Original array
+#[0 1 2 3 4 5 6]
+#First array elements raised to powers from second array, element-wise:
+#[ 0 1 8 27 64 125 216]
+
+import pandas as pd
+
+df=pd.DataFrame({})
