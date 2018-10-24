b758696a82968bbfd8eec67221e5e04c7c3fc2e2
shruti <shruti.kerkar@bridgelabz.com>
2018-10-24 15:10:29 +0530

    Updated commit

--- PythonLibraries/Pandas/NumberOfAttemptsInExaminationIsGreaterThan2.py
+++ PythonLibraries/Pandas/NumberOfAttemptsInExaminationIsGreaterThan2.py
@@ -1,6 +1,5 @@
 
-#Write a Python program to select the specified columns and rows from a given data frame.
-#Select 'name' and 'score' columns in rows 1, 3, 5, 6 from the following data frame.
+#Write a Python program to select the rows where the number of attempts in the examination is greater than 2.
 #exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
 #'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
 #'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
@@ -9,7 +8,6 @@
 
 import pandas as pd
 import numpy as np
-
 exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
 'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
 'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
@@ -18,7 +16,4 @@ exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Micha
 labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
 df=pd.DataFrame(exam_data,index=labels)
-
 print(df)
-
-print(df.iloc[[1,3,5,6],[1,3]])
