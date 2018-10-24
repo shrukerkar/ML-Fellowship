ff67abb2b689ea3b87f067a9677671ec56069385
shruti <shruti.kerkar@bridgelabz.com>
2018-10-24 16:47:28 +0530

    commit

--- PythonLibraries/Pandas/SelectRowsWhereScoreIsMissing.py
+++ PythonLibraries/Pandas/SelectRowsWhereScoreIsMissing.py
@@ -1,11 +1,11 @@
 
-#Write a Python program to count the number of rows and columns of a DataFrame.
-#Sample data:
+
+# Write a Python program to select the rows where the score is missing, i.e. is NaN.
 #exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
 #'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
 #'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
 #'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
-#labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+#labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
 import pandas as pd
 import numpy as np
@@ -17,15 +17,11 @@ exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Micha
 
 labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
-df=pd.DataFrame(exam_data,index=labels)
 
+df=pd.DataFrame(exam_data,index=labels)
 print(df)
 
-total_rows=len(df.axes[0])
-total_col=len(df.axes[1])
+print(df['score'].isnull)
 
-print("total number of rows:")
-print(total_rows)
-print("total number of columns:")
-print(total_col)
+
 
