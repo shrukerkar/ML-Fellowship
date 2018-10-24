a2e4b48dd21893f2736eb11f93a9c27a721957fc
shruti <shruti.kerkar@bridgelabz.com>
2018-10-24 18:38:17 +0530

    Updated commit

---
+++ PythonLibraries/Pandas/SortDataframeDescendingOrderAscendingOrder.py
@@ -0,0 +1,26 @@
+
+#Write a Python program to sort the DataFrame first by 'name' in descending order, then by 'score' in ascending order.
+#exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+#'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+#'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+#'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+#labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+#Values for each column will be:
+#name : "Suresh", score: 15.5, attempts: 1, qualify: "yes", label: "k"
+
+import pandas as pd
+import numpy as np
+
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df=pd.DataFrame(exam_data,index=labels)
+print(df)
+
+df.sort_values(by=['name','score'],ascending=(False,True))
+print(df)
