dcaee2dd97bbecd82114ed7a36008bf177ab23b4
shruti <shruti.kerkar@bridgelabz.com>
2018-10-25 12:56:50 +0530

    Updated commit

---
+++ PythonLibraries/Pandas/InsertNewColumnToExistingDataFrame.py
@@ -0,0 +1,28 @@
+
+# Write a Python program to insert a new column in existing DataFrame.
+#exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+#'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+#'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+#'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+#labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+
+import pandas as pd
+import numpy as np
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df=pd.DataFrame(exam_data,index=labels)
+print("Original Dataframe:")
+print(df)
+
+#Adding Colours column to existing dataframe
+colours=['red','black','blue','orange','pink','green','white','purple','magenta','Grey']
+df['colours']=colours
+print("Adding Colours column to Original DataFrame:")
+print(df)
