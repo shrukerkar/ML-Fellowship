cba723540087ca5def92361ff6ce87c22dc7a1b7
shruti <shruti.kerkar@bridgelabz.com>
2018-10-27 17:49:33 +0530

    commit

--- PythonLibraries/Matplotlib/PieChart.py
+++ PythonLibraries/Matplotlib/PieChart.py
@@ -7,7 +7,7 @@
 import matplotlib.pyplot as plt
 
 
-languages=[Java, Python, PHP, JavaScript,C#,C++]
+languages=['Java', 'Python', 'PHP', 'JavaScript','C#','C++']
 popularity = [ 22.2, 17.6, 8.8,8,7.7,6.7]
 colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"]
 
---
+++ PythonLibraries/Matplotlib/PieChartWithTitlePopularity.py
@@ -0,0 +1,19 @@
+
+#Write a Python programming to create a pie chart with a title of the popularity of programming Languages.
+#Sample data:
+#Programming languages: Java, Python, PHP, JavaScript, C#, C++
+#Popularity: 22.2, 17.6, 8.8, 8, 7.7, 6.7
+
+import matplotlib.pyplot as plt
+
+languages=['Java', 'Python', 'PHP', 'JavaScript','C#','C++']
+popularity = [22.2,17.6,8.8,8,7.7,6.7]
+
+color=['red', 'gold', 'yellowgreen', 'blue', 'lightcoral', 'lightskyblue']
+
+explode =(0.1, 0, 0, 0,0,0)
+
+plt.pie(popularity, explode=explode, labels=languages, colors=color,autopct='%1.1f%%', shadow=True, startangle=140)
+
+plt.title("Popularity of languages")
+plt.show()
