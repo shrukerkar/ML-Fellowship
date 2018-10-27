c264df19e7bbdb890f486c0ffba5adb1a745e3d7
shruti <shruti.kerkar@bridgelabz.com>
2018-10-27 11:46:52 +0530

    commit

---
+++ PythonLibraries/Matplotlib/BarChartUniformColor.py
@@ -0,0 +1,29 @@
+
+# Write a Python programming to display a bar chart of the popularity of programming Languages. Use uniform color.
+#Sample data:
+#Programming languages: Java, Python, PHP, JavaScript, C#, C++
+#Popularity: 22.2, 17.6, 8.8, 8, 7.7, 6.7
+
+
+import matplotlib.pyplot as plt
+
+programming_languages= ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
+popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+
+x=[i for i,_ in enumerate(programming_languages)]
+
+plt.bar(x,popularity,color=(0.4,0.6,0.8,1.0))
+
+plt.xlabel("languages")
+plt.ylabel("popularity")
+plt.title("Popularity Of Programming Languages:")
+
+plt.xticks(x,programming_languages)
+
+plt.minorticks_on()
+plt.grid(which='major',linestyle='-',linewidth='0.5',color='red')
+plt.grid(which='minor',linestyle=':',linewidth='0.5',color='black')
+
+plt.show()
+
+
