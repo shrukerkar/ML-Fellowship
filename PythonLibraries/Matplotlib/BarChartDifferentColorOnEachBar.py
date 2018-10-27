036fc1f36c6d99b88ff05bad33d2613b91b363c5
shruti <shruti.kerkar@bridgelabz.com>
2018-10-27 11:54:06 +0530

    commit

---
+++ PythonLibraries/Matplotlib/BarChartDifferentColorOnEachBar.py
@@ -0,0 +1,27 @@
+
+#Write a Python programming to display a bar chart of the popularity of programming Languages. Use different color for each bar.
+#Sample data:
+#Programming languages: Java, Python, PHP, JavaScript, C#, C++
+#Popularity: 22.2, 17.6, 8.8, 8, 7.7, 6.7
+
+
+import matplotlib.pyplot as plt
+
+programming_languages= ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
+popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+
+x=[i for i,_ in enumerate(programming_languages)]
+
+plt.bar(x,popularity,color=['red','black','blue','green','yellow','cyan'])
+
+plt.xlabel("languages")
+plt.ylabel("popularity")
+plt.title("Popularity Of Programming Languages:")
+
+plt.xticks(x,programming_languages)
+
+plt.minorticks_on()
+plt.grid(which='major',linestyle='-',linewidth='0.5',color='red')
+plt.grid(which='minor',linestyle=':',linewidth='0.5',color='black')
+
+plt.show()
