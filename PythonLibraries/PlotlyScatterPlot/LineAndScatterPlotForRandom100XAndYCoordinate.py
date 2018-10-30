067a2c2c28362de9686dc3b0bf338915aa12c8d0
shruti <shruti.kerkar@bridgelabz.com>
2018-10-30 15:15:45 +0530

    commit

---
+++ PythonLibraries/PlotlyScatterPlot/LineAndScatterPlotForRandom100XAndYCoordinate.py
@@ -0,0 +1,6 @@
+
+# Write a program to draw line and scatter plots for random 100 x and y coordinates
+
+import plotly
+
+plotly.tools.set_credentials_file(username='shrukerkar',api_key='Xz0m9hmG79qoKrR9kam6')
--- PythonLibraries/PlotlyScatterPlot/ScatterPlotForRandom1000XAndYCoordinates.py
+++ PythonLibraries/PlotlyScatterPlot/ScatterPlotForRandom1000XAndYCoordinates.py
@@ -6,6 +6,10 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 import numpy as np
 
+import plotly
+
+plotly.tools.set_credentials_file(username='shrukerkar',api_key='Xz0m9hmG79qoKrR9kam6')
+
 N=1000
 
 x_random=np.random.randn(N)
