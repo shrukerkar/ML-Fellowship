893fdacc7ac3e9d4c235aee9a33bb972c381ecd7
shruti <shruti.kerkar@bridgelabz.com>
2018-10-30 12:19:26 +0530

    commit

---
+++ PythonLibraries/Seaborn/SeabornBoxPlotOfLifeExpectancy.py
@@ -0,0 +1,17 @@
+
+#Write a program to draw box plot of life expectancy by continent for a data set given in a url
+#  https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data_url= "https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv"
+
+df=pd.read_csv(data_url)
+
+print(df.head())
+
+x=sns.barplot(x="continent",y="lifeExp",data=df)
+
+plt.show()
