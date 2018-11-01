a615cec054c86c7b3f76d6fba2aa1217c92315fe
shruti <shruti.kerkar@bridgelabz.com>
2018-11-01 19:12:00 +0530

    commit

--- Regression/SimpleLinearRegression/LinearRegressionEx1.py
+++ Regression/SimpleLinearRegression/LinearRegressionEx1.py
@@ -7,28 +7,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df=pd.read_csv("/home/shruti/Downloads/simple-linear-regression/train.csv")
-#print(df.head())
-#print(df.shape)
-#print(df.info())
-#print(df.describe())
-#print(df.columns)
+#Read data set
+df_train=pd.read_csv("/home/shruti/Downloads/simple-linear-regression/train.csv")
+df_test=pd.read_csv("/home/shruti/Downloads/simple-linear-regression/test.csv")
+
+
+# Prints first 5 records
+print(df_train.head())
+print(df_test.head())
+# prints shape of data
+print(df_train.shape)
+print(df_test.shape)
+# Dataset information
+print(df_train.info())
+print(df_test.info())
+
+# Describe dataset
+print(df_train.describe())
+print(df_test.describe())
+#Print columns
+print(df_train.columns)
+print(df_test.columns)
 
 #sns.pairplot(df)
 #sns.distplot(df['x'])
 #plt.show()
 
-X=df['x']
+
+#Collecting X and Y
+X=df_train['x']
 print(X)
-Y=df['y']
+Y=df_train['y']
 print(Y)
 
+# mean X and Y
 x_mean=np.mean(X)
 print(x_mean)
 y_mean=np.mean(Y)
 print(y_mean)
 
+#Total number of values
+
 m=len(X)
 print(m)
 
+numerator=0
+denominator=0
 
+for i in range (m):
+    numerator=(X[i]-x_mean)*(Y[i]-y_mean)
+    denominator=(X[i]-x_mean)**2
+
+b1=numerator/denominator
+b0=y_mean-(b1*x_mean)
+print(b1,b0)
+
+
+x_max=np.max(X)
+x_min=np.min(X)
+
+x=np.linspace(x_min,x_max,1000)
+y=b0+b1*x
+
+plt.plot(x,y,color='r',label='regression line')
+plt.plot(X,Y,c='g',label='scatter plot')
+plt.legend()
+plt.show()
+
+rmse=0
+for i in range(m):
+    y_pred=b0+b1*X[i]
