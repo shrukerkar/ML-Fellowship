b251f22b1c55da6b50b598845cfe2898bfffcd02
shruti <shruti.kerkar@bridgelabz.com>
2018-11-27 13:15:03 +0530

    Updated commit

--- MLUsingLibraries/Regression/DecisionTreeRegression/DecisionTreeEx1.py
+++ MLUsingLibraries/Regression/DecisionTreeRegression/DecisionTreeEx1.py
@@ -9,23 +9,33 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import classification_report,confusion_matrix
+from sklearn import metrics
 
 #Read dataset
 
 data=pd.read_csv("/home/shruti/Downloads/Position_Salaries.csv")
 print(data.head())
 
-X=data.iloc[:,1:2].values
-print(X)
+#Dependent and independent variable
 
-y=data.iloc[:,2].values
+#X=data.drop('Salary',axis=1)
+#print(X)
+X=data.iloc[:,1:2];
+print(X)
+y=data['Salary']
 print(y)
+#y=data.iloc[:,2]
+#print(y)
+
+#Train test spliting
 
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.20)
 print(X_train)
 print(X_test)
 print(y_train)
 print(y_test)
+
+#Training and making predictions
 
 regressor=DecisionTreeRegressor()
 regressor.fit(X_train,y_train)
@@ -34,4 +44,14 @@ y_pred=regressor.predict(X_test)
 
 print(confusion_matrix(y_test,y_pred))
 print(classification_report(y_test,y_pred))
+
+
+df=pd.DataFrame({'Actual':y_test,'Predicted':y_pred})
+print(df)
+
+#Evaluating Algorithm
+
+print("Mean absolute error:",metrics.mean_absolute_error(y_test,y_pred))
+print("Mean squared error:",metrics.mean_squared_error(y_test,y_pred))
+print("Root mean squared error:",np.sqrt(metrics.mean_squared_error(y_test,y_pred)))
 
